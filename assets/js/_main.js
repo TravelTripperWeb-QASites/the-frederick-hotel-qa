ef520274e7e421b8c3576de3910495d331505026
mtufailshaikh <mtufail@traveltripper.com>
2017-09-07 18:00:38 +0530

    added dynamic map to both pages home and location

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -99,13 +99,7 @@ $(document).ready(function(){
        });
     });
 
-    function initMap() {
-      // Create a map object and specify the DOM element for display.
-      var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-    }
+     
 
   
 }); 
