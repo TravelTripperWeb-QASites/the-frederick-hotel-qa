6fbefea3a3ae6dc6179564e6bf67b0ddb3aa0ac2
kiran-TT-web <kmekala@traveltripper.com>
2017-09-08 18:07:05 +0530

    gallery page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -106,7 +106,34 @@ $(document).ready(function(){
        });
     });
 
+
+//gallery filter
  
+$(".filter").on("click", function () {
+         var $this = $(this);
+         // if we click the active tab, do nothing
+         if (!$this.hasClass("active")) {
+           $(".filter").removeClass("active");
+           $this.addClass("active"); // set the active tab
+           var $filter = $this.data("rel"); // get the data-rel value from selected tab and set as filter
+           $filter == 'all' ? // if we select "view all", return to initial settings and show all
+             $(".fancybox").attr("data-fancybox-group", "gallery").not(":visible").fadeIn()
+             : // otherwise
+             $(".fancybox").fadeOut(0).filter(function () {
+               return $(this).data("filter") == $filter; // set data-filter value as the data-rel value of selected tab
+             }).attr("data-fancybox-group", $filter).fadeIn(1000); // set data-fancybox-group and show filtered elements
+         } // if
+       }); // on
+
+          $('#lightgallery').lightGallery({
+           selector: '.item',
+           counter: false,
+           thumbnail: false
+         });
+
+
+
+
   //plus minus
   $(".incr-btn").on("click", function (e) {
     var $button = $(this);
